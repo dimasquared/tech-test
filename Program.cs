988d169e34e3e06f95c48d6b420e71f33c08d002
Valentine Dmitrieva <dimasquared@gmail.com>
2023-03-17 21:07:37 +0100

    ref: add class Dot, do some changes in class Triangle

--- Program.cs
+++ Program.cs
@@ -1,122 +1,132 @@
-﻿
-
-class App
+﻿class App
 {
     static void Main()
     {
-        double xA = Triangle.ReadCoordinate("A", "x");
-        double yA = Triangle.ReadCoordinate("A", "y");
-        double xB = Triangle.ReadCoordinate("B", "x");
-        double yB = Triangle.ReadCoordinate("B", "y");
-        double xC = Triangle.ReadCoordinate("C", "x");
-        double yC = Triangle.ReadCoordinate("C", "y");
-        
+        Dot dotA = new Dot("A");
+        Dot dotB = new Dot("B");
+        Dot dotC = new Dot("C");
+        dotA.ReadCoordinates();
+        dotB.ReadCoordinates();
+        dotC.ReadCoordinates();
+
         Console.WriteLine("");
-        
-        double ab = Triangle.CalculateSideLength("ab", xA, yA, xB, yB);
-        double bc = Triangle.CalculateSideLength("bс", xB, yB, xC, yC);
-        double ac = Triangle.CalculateSideLength("aс", xA, yA, xC, yC);
 
-        Triangle.IfEquilateral(ab, bc, ac);
-        Triangle.IfIsosceles(ab, bc, ac);
-        Triangle.IfRight(ab, bc, ac);
+        Triangle triangle = new Triangle(dotA, dotB, dotC);
+        Console.WriteLine($"Lenght of {nameof(triangle.AB)} is: {triangle.AB}");
+        Console.WriteLine($"Lenght of {nameof(triangle.BC)} is: {triangle.BC}");
+        Console.WriteLine($"Lenght of {nameof(triangle.AC)} is: {triangle.AC}\n");
 
-        double perimeter = Triangle.CalculatePerimeter(ab, bc, ac);
-        
-        
-        
-        Triangle.FindEvenNumbers(perimeter);
-        
-        Console.Write("\nPress any key to close...");
-        Console.ReadKey();
-
-    }
-}
-
-
-class Triangle
-{
-    public static double ReadCoordinate(string dotName, string coordinate)
-    {
-        double x = 0;
-        
-        do
-        {
-            Console.WriteLine($"Enter {coordinate} coordinate of dot {dotName}");
-        } while (!Double.TryParse(Console.ReadLine(), out x));
-
-        return x;
-    }
-
-    public static double CalculateSideLength(string sideName, double x1, double y1, double x2, double y2)
-    {
-        double sideLength = 0;
-   
-        sideLength = Math.Pow(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2), 0.5);
-        Console.WriteLine($"Lenght of {sideName.ToUpper()} is: {sideLength}");
-        
-        return sideLength;
-    }
-
-    public static void IfEquilateral(double sideLength1, double sideLength2, double sideLength3)
-    {
-        if (sideLength1 == sideLength2 && sideLength2 == sideLength3)
-        {
-            Console.WriteLine("\nTriangle IS 'Equilateral'");
-        }
-        else Console.WriteLine("\nTriangle IS NOT 'Equilateral'");
-    }
-
-    public static void IfIsosceles(double sideLength1, double sideLength2, double sideLength3)
-    {
-        if (sideLength1 == sideLength2 || sideLength1 == sideLength3 || sideLength3 == sideLength2)
-        {
-            Console.WriteLine("Triangle IS 'Isosceles'");
-        }
-        else Console.WriteLine("Triangle IS NOT 'Isosceles'");
-    }
-
-    public static void IfRight(double sideLength1, double sideLength2, double sideLength3)
-    {
-        if (sideLength1 == sideLength2 && sideLength2 == sideLength3)
-        {
-            Console.WriteLine("Triangle IS NOT 'Right'");
-        }
+        if (triangle.IsEquilateral())
+            Console.WriteLine("Triangle IS 'Equilateral'");
         else
-        {
-            List<double> sides = new List<double>() { sideLength1, sideLength2, sideLength3 };
-            List<double> sideSquared = new List<double>();
-            
-            foreach (var side in sides)
-            {
-                sideSquared.Add(Math.Pow(side, 2));
-            }
+            Console.WriteLine("Triangle IS NOT 'Equilateral'");
 
-            double maxSideSquared = sideSquared.Max();
-            double sumSideSquared = sideSquared.Sum();
-            double sumLessSideSquared = sumSideSquared - maxSideSquared;
+        if (triangle.IsIsosceles())
+            Console.WriteLine("Triangle IS 'Isosceles'");
+        else
+            Console.WriteLine("Triangle IS NOT 'Isosceles'");
 
-            if (maxSideSquared == sumLessSideSquared)
-            {
-                Console.WriteLine("Triangle IS 'Right'\n");
-            }
-            else Console.WriteLine("Triangle IS NOT 'Right'\n");
-        }
-    }
+        if (triangle.IsRight())
+            Console.WriteLine("Triangle IS 'Right'\n");
+        else
+            Console.WriteLine("Triangle IS NOT 'Right'\n");
 
-    public static double CalculatePerimeter(double sideLength1, double sideLength2, double sideLength3)
-    {
-        double perimeter = sideLength1 + sideLength2 + sideLength3;
-        Console.WriteLine($"Perimeter: {perimeter}\n");
+        Console.WriteLine($"Perimeter: {triangle.Perimeter}\n");
+        Console.WriteLine("Parity numbers in range from 0 to triangle perimeter:");
 
-        return perimeter;
-    }
-
-    public static void FindEvenNumbers(double perimeter)
-    {
-        for (int i = 0; i < perimeter; i += 2)
+        for (int i = 0; i < triangle.Perimeter; i += 2)
         {
             Console.WriteLine(i);
         }
+
+        Console.Write("\nPress any key to close...");
+        Console.ReadKey();
+    }
+}
+
+class Dot
+{
+    public string Name { get; }
+    public double X { get; private set; }
+    public double Y { get; private set; }
+
+    public Dot(string name)
+    {
+        Name = name;
+    }
+
+    public void ReadCoordinates()
+    {
+        X = ReadCoordinate("x");
+        Y = ReadCoordinate("y");
+    }
+
+    private double ReadCoordinate(string axisName)
+    {
+        double result;
+
+        do
+        {
+            Console.WriteLine($"Enter {axisName} coordinate of dot {Name}");
+        } while (!Double.TryParse(Console.ReadLine(), out result));
+
+        return result;
+    }
+}
+
+class Triangle
+{
+    public double AB { get; }
+    public double BC { get; }
+    public double AC { get; }
+    public double Perimeter { get; }
+
+    public Triangle(Dot a, Dot b, Dot c)
+    {
+        AB = CalculateSideLength(a, b);
+        BC = CalculateSideLength(b, c);
+        AC = CalculateSideLength(a, c);
+        Perimeter = CalculatePerimeter();
+    }
+
+    public bool IsEquilateral()
+    {
+        bool result = AB == BC && BC == AC;
+        return result;
+    }
+
+    public bool IsIsosceles()
+    {
+        bool result = AB == BC || AB == AC || BC == AC;
+        return result;
+    }
+
+    public bool IsRight()
+    {
+        if (IsEquilateral())
+        {
+            return false;
+        }
+
+        List<double> sidesSquared = new List<double>() { Math.Pow(AB, 2), Math.Pow(BC, 2), Math.Pow(AC, 2) };
+
+        double maxSideSquared = sidesSquared.Max();
+        double sumSidesSquared = sidesSquared.Sum();
+        double sumSmallerSidesSquared = sumSidesSquared - maxSideSquared;
+
+        bool result = maxSideSquared == sumSmallerSidesSquared;
+        return result;
+    }
+
+    private double CalculatePerimeter()
+    {
+        double perimeter = AB + BC + AC;
+        return perimeter;
+    }
+
+    private double CalculateSideLength(Dot dot1, Dot dot2)
+    {
+        double result = Math.Pow(Math.Pow(dot2.X - dot1.X, 2) + Math.Pow(dot2.Y - dot1.Y, 2), 0.5);
+        return result;
     }
 }
